4028674e7c9f9ffcd74c5b7d93c5cae705bd8ee2
TrawkaO <qscokmvbn@mail.ru>
2023-06-26 11:16:36 +0300

    Добавьте файлы проекта.

---
+++ DZ_7/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace DZ_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var menu = 1;
+            do
+            {
+                Console.WriteLine("Выберите ДЗ " +
+                                  "\n 1. Плащадь квадрата " +
+                                  "\n 2. Площадь круга " +
+                                  "\n 3. Площадь Треугольника " );
+                Console.Write("Ваш выбор: ");
+                string golos = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (golos)
+                {
+                    case "1":
+
+                        Console.WriteLine("Введите длинну стороны квадрата: ");
+                        double side = double.Parse(Console.ReadLine());
+                        Square square = new Square(side);
+                        Console.WriteLine($"Площадь квардата: {square.GetSqure()}");
+
+                        Console.WriteLine("Возврат в меню? \n 1. Да \n 2. Нет");
+                        Console.Write("Ваш выбор: ");
+                        menu = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Введите радиус круга: ");
+                        double radius = double.Parse(Console.ReadLine());
+                        Circle circle = new Circle(radius);
+                        Console.WriteLine($"Площадь Круга: {circle.GetSquare()}");
+
+                        Console.WriteLine("Возврат в меню? \n 1. Да \n 2. Нет");
+                        Console.Write("Ваш выбор: ");
+                        menu = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите высоту треугольника : ");
+                        double heght = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите длинну треугольника: ");
+                        double length = double.Parse(Console.ReadLine());
+                        Delta delta = new Delta(heght, length);
+
+                        Console.WriteLine($"Площадь Треугольника: {delta.GetSquare()}");
+
+                        Console.WriteLine("Возврат в меню? \n 1. Да \n 2. Нет");
+                        Console.Write("Ваш выбор: ");
+                        menu = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        break;
+                }
+
+
+
+            } while (menu == 1);
+          
+        }
+    }
+
+    public class Figure
+    {
+        public double GetSquare()
+        {
+            return 0;
+        }
+    }
+
+    public class Square : Figure
+    {
+        private double side;
+
+        public Square(double side_q)
+        {
+            side = side_q;  
+        }
+
+        public double GetSqure()
+        {
+            return side * side;
+        }
+    }
+
+    public class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double radius_q)
+        {
+            radius = radius_q;
+        }
+
+        public double GetSquare()
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+
+    public class Delta : Figure
+    {
+        private double height;
+        private double length;
+
+        public Delta(double height_q, double length_q)
+        {
+            height = height_q;
+            length = length_q;
+        }
+
+        public double GetSquare()
+        {
+            return length * height * 0.5;
+        }
+
+    }
+
+    
+}
---
+++ DZ_7_2/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace DZ_7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var menu = 1;
+            do
+            {
+                Console.WriteLine("Кого создаём? " +
+                                  "\n 1. Создать Котеина " +
+                                  "\n 2. Создать Хрюнделя ");
+                Console.Write("Ваш выбор: ");
+                string golos = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (golos)
+                {
+                    case "1":
+                        Animal animal = new Cat();
+                        Console.WriteLine("Как зовут? ");
+                        animal.Name = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Чем будем кормить? " +
+                                          "\n 1. Хлебом с картошкой " +
+                                          "\n 2. Паштет свинной ");
+                        Console.Write("Ваш выбор: ");
+                        int eat = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Сколько порций?");
+                        Console.Write("Ваш выбор: ");
+
+                        int eat_count = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        if (eat == 1)
+                        {
+                            animal.Feed(FoodType.PigFood,eat_count);
+                        }
+                        else
+                        {
+                            animal.Feed(FoodType.CatFood,eat_count);
+                        }
+                        Console.WriteLine();
+                        animal.SayHello();
+                        Console.WriteLine();
+
+                        Console.WriteLine("Возврат в меню? \n 1. Да \n 2. Нет");
+                        Console.Write("Ваш выбор: ");
+                        menu = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+
+
+                        Animal animal2 = new Pig();
+                        Console.WriteLine("Как зовут? ");
+                        animal2.Name = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Чем будем кормить? " +
+                                          "\n 1. Хлебом с картошкой " +
+                                          "\n 2. Паштет свинной ");
+                        Console.Write("Ваш выбор: ");
+                        int eat2 = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Сколько порций?");
+                        Console.Write("Ваш выбор: ");
+                        int eat_count2 = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                        if (eat2 == 1)
+                        {
+                            animal2.Feed(FoodType.PigFood, eat_count2);
+                        }
+                        else
+                        {
+                            animal2.Feed(FoodType.CatFood, eat_count2);
+                        }
+                        Console.WriteLine();
+                        animal2.SayHello();
+                        Console.WriteLine();
+
+                        Console.WriteLine("Возврат в меню? \n 1. Да \n 2. Нет");
+                        Console.Write("Ваш выбор: ");
+                        menu = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        break;
+                }
+
+
+
+            } while (menu == 1);
+        }
+
+        public abstract class Animal
+        {
+            public string Name   { get; set; }
+            protected double Satiety { get; set; }
+
+            public virtual void Feed(FoodType foodType, int count)
+            {
+                if (foodType != GetFoodType())
+                {
+                    Console.WriteLine("Цэ не моя еда");
+                    return;
+                }
+
+                Satiety += count;
+                Console.WriteLine("Майн еда Om-nom-nom");
+            }
+
+            public void SayHello()
+            {
+                Console.WriteLine($"Привет {Name}");
+            }
+            protected abstract FoodType GetFoodType();
+        }
+
+        public class Cat :Animal
+        {
+            protected override FoodType GetFoodType()
+            {
+                return FoodType.CatFood;
+            }
+        }
+
+        public class Pig : Animal
+        {
+            protected override FoodType GetFoodType()
+            {
+                return FoodType.PigFood;
+            }
+        }
+
+        public enum FoodType
+        {
+            PigFood,
+            CatFood
+        }
+    }
+}
